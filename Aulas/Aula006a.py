4c294d9511c2fb9c1cc9e9d4d7c2efbd0203b75c
atilajp@gmail.com <Git@qaz123>
2020-05-02 15:54:15 -0300

    Adicionado arquivos de exercicio e aula06

---
+++ Aulas/Aula006a.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um numero! '))
+n2 = int(input('Digite o segundo numero! '))
+print('A soma entre {} e {} é {}'.format(n1, n2, n1+n2))
---
+++ Aulas/au001.py
@@ -0,0 +1,3 @@
+name = input('Qual o seu nome: ')
+print('Ola Paser em te conhecer ',name)
+
