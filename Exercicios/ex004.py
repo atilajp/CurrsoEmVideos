ba1821812d9a6aafa9787c8a5d9f439bbd13a769
atilajp@gmail.com <Git@qaz123>
2020-05-06 17:41:48 -0300

    Adicionado desafio 004

---
+++ Exercicios/ex004.py
@@ -0,0 +1,23 @@
+algo = input('Digite Algo')
+print('O tipo primitivo desse valor é {}'.format(type(algo)))
+print('É maiúsculo?', algo.isupper())
+print('É minúsculo?', algo.islower())
+print('É Alfanumérico? ', algo.isalnum())
+print('É alfabético? ', algo.isalpha())
+print('É ASCII? ', algo.isascii())
+print('É Decimal? ', algo.isdecimal())
+print('É um digito? ', algo.isdigit())
+print('isidentifier?', algo.isidentifier())
+print('É minúsculo?', algo.islower())
+print('É Numérico?', algo.isnumeric())
+print('É imprimível?', algo.isprintable())
+print('É sp espaços?', algo.isspace())
+print('É um titulo Primeira letra Maiúsculas?', algo.istitle())
+
+
+
+
+
+
+
+
